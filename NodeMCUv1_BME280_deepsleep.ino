35e75886615254989354c4acdb43570d42974eab
PORNPASOK SOOKYEN <ton350d@gmail.com>
2020-03-26 16:42:17 +0700

    Create NodeMCUv1_BME280_deepsleep.ino

---
+++ NodeMCUv1_BME280_deepsleep.ino
@@ -0,0 +1,135 @@
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#endif
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+ADC_MODE(ADC_VCC);
+
+const char* ssid = "tono.io";
+const char* password = "0863741677";
+
+const char* serverName = "http://tonofarm.herokuapp.com/esp-post-data.php";
+
+String apiKeyValue = "tonofarm.io";
+String sensorName = "BME280";
+String sensorLocation = "13.7868709,100.5457628";
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme;  // I2C
+//Adafruit_BME280 bme(BME_CS);  // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);  // software SPI
+
+float v;
+int value = 0;
+void connect() {
+   // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  // VCC
+  v = ESP.getVcc() / 1000.0;
+  Serial.print("VCC: ");
+  Serial.println(v);
+  
+  // (you can also pass in a Wire library object like &Wire2)
+  bool status = bme.begin(0x76);
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
+    while (1);
+  }
+  
+  Serial.println("Timer set to 30 seconds (timerDelay variable), it will take 30 seconds before publishing the first reading.");
+  // Use WiFiClient class to create TCP connections
+  
+  if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverName);
+
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+      // Prepare your HTTP POST request data
+      String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                            + "&location=" + sensorLocation + "&value1=" + String(bme.readTemperature())
+                            + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure()/100.0F) + "&value4=" + v + "";
+      Serial.print("httpRequestData: ");
+      Serial.println(httpRequestData);
+
+      // You can comment the httpRequestData variable above
+      // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+      //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+      // Send HTTP POST request
+      int httpResponseCode = http.POST(httpRequestData);
+
+      // Show Connect Status
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+      delay(1000);
+
+      // If you need an HTTP request with a content type: text/plain
+      //http.addHeader("Content-Type", "text/plain");
+      //int httpResponseCode = http.POST("Hello, World!");
+
+      // If you need an HTTP request with a content type: application/json, use the following:
+      //http.addHeader("Content-Type", "application/json");
+      //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+   }
+   else {
+    Serial.println("WiFi Disconnected");
+   }
+}
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.setTimeout(2000);
+  // Wait for serial to initialize.
+  while (!Serial) { }
+
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+  Serial.println("Running Deep Sleep Firmware!");
+  Serial.println("-------------------------------------");
+
+  connect();
+
+  Serial.println("Sleeping 60 seconds ..");
+  // Deep Sleep 60 seconds
+  ESP.deepSleep(58e6); // 60e6 is 60 microsecondsESP.
+}
+
+void loop() {
+}
